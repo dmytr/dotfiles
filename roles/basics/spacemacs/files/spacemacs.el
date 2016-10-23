617f05cffd402ebf1ee86a12dc523b9187ac7733
dmytr <dmytr@users.noreply.github.com>
2016-10-23 14:08:46 +0200

    Few more Haskell deps

--- roles/basics/spacemacs/files/spacemacs.el
+++ roles/basics/spacemacs/files/spacemacs.el
@@ -53,7 +53,9 @@ values."
      javascript
      typescript
      yaml
-     (haskell :variables haskell-completion-backend 'ghc-mod)
+     (haskell :variables
+              haskell-completion-backend 'intero
+              haskell-enable-hindent-style "johan-tibell")
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
