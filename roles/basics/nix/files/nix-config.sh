0337b18e9d536c73b455685fa651cddd15b14e00
dmytr <dmytr@users.noreply.github.com>
2018-03-23 10:45:12 +0100

    Safer env variables

--- roles/basics/nix/files/nix-config.sh
+++ roles/basics/nix/files/nix-config.sh
@@ -2,6 +2,17 @@ source $HOME/.nix-profile/etc/profile.d/nix.sh
 
 export NIX_PATH="$NIX_PATH":"local=$HOME/.local/nixpkgs"
 
-export XDG_DATA_DIRS=$HOME/.nix-profile/share:$XDG_DATA_DIRS
+NIX_XDG_DATA_DIRS=$HOME/.nix-profile/share
+if [ -z "$XDG_DATA_DIRS" ]; then
+    export XDG_DATA_DIRS="$NIX_XDG_DATA_DIRS"
+else
+    export XDG_DATA_DIRS="$NIX_XDG_DATA_DIRS":"$XDG_DATA_DIRS"
+fi
 
-export LIBGL_DRIVERS_PATH=/nix/store/1yawik475cqd2d0pczg11q69bc2x097f-mesa-noglu-17.3.3-drivers/lib/dri
+NIX_LIBGL_DRIVERS_PATH=/nix/store/1yawik475cqd2d0pczg11q69bc2x097f-mesa-noglu-17.3.3-drivers/lib/dri
+if [ -z "$LIBGL_DRIVERS_PATH" ]; then
+    export LIBGL_DRIVERS_PATH="$NIX_LIBGL_DRIVERS_PATH"
+else
+    export LIBGL_DRIVERS_PATH="$NIX_LIBGL_DRIVERS_PATH":"$LIBGL_DRIVERS_PATH"
+fi
+
