cb0744a0bed2a63e0a1a09bb2dd3bf5b7249b66a
dmytr <dmytr@users.noreply.github.com>
2018-03-23 10:19:41 +0100

    GL libs load path for Nix GUI apps

--- roles/basics/nix/files/nix-config.sh
+++ roles/basics/nix/files/nix-config.sh
@@ -4,4 +4,4 @@ export NIX_PATH="$NIX_PATH":"local=$HOME/.local/nixpkgs"
 
 export XDG_DATA_DIRS=$HOME/.nix-profile/share:$XDG_DATA_DIRS
 
-#export LIBGL_DRIVERS_PATH=/nix/store/1yawik475cqd2d0pczg11q69bc2x097f-mesa-noglu-17.3.3-drivers/lib/dri
+export LIBGL_DRIVERS_PATH=/nix/store/1yawik475cqd2d0pczg11q69bc2x097f-mesa-noglu-17.3.3-drivers/lib/dri
