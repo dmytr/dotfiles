e36847a6786eeb33e6c83064339a36a248aa84d1
dmytr@users.noreply.github.com <dmytr>
2018-03-23 01:43:32 +0100

    Nix tweaks

--- roles/basics/nix/files/nix-config.sh
+++ roles/basics/nix/files/nix-config.sh
@@ -1,3 +1,7 @@
 source $HOME/.nix-profile/etc/profile.d/nix.sh
 
 export NIX_PATH="$NIX_PATH":"local=$HOME/.local/nixpkgs"
+
+export XDG_DATA_DIRS=$HOME/.nix-profile/share:$XDG_DATA_DIRS
+
+#export LIBGL_DRIVERS_PATH=/nix/store/1yawik475cqd2d0pczg11q69bc2x097f-mesa-noglu-17.3.3-drivers/lib/dri
