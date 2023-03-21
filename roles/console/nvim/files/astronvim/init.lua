5c3b3da85b050c2fb8a0dddd43c93f2d809df78e
dmytr <dmytr@users.noreply.github.com>
2023-03-21 22:17:12 +0100

    Fix colors

--- roles/console/nvim/files/astronvim/init.lua
+++ roles/console/nvim/files/astronvim/init.lua
@@ -13,5 +13,28 @@ return {
       end,
     },
   },
+
+  heirline = {
+    colors = function(colors)
+      local status = require "astronvim.utils.status"
+      local C = status.env.fallback_colors
+      local get_hlgroup = require("astronvim.utils").get_hlgroup
+
+      local bg1 = get_hlgroup("GruvboxBg1", nil)
+      local fg1 = get_hlgroup("GruvboxFg4", nil)
+
+      if bg1 ~= nil then
+        colors.tabline_bg = bg1.fg
+        colors.section_bg = bg1.fg
+        colors.bg = bg1.fg
+      end
+
+      if fg1 ~= nil then
+        colors.section_fg = fg1.fg
+      end
+
+      return colors
+    end,
+  },
 }
 
